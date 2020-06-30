e8c175c025659c83d9c260a32411a3631bb48435
Sedki Youzbechi <mohamed.sedki.youzbechi@dotit-medu.com>
2020-06-30 17:32:16 +0100

    Add image to the local notification but there are much error|Not working..

--- lib/utils/local_notification_helper.dart
+++ lib/utils/local_notification_helper.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:localpushnotifications/utils/save_image.dart';
 import 'package:meta/meta.dart';
 
 NotificationDetails get _noSound {
@@ -29,8 +34,8 @@ NotificationDetails get _ongoing {
     'your channel description',
     importance: Importance.Max,
     priority: Priority.High,
-    ongoing: false,  //make the notification don't popup
-    autoCancel: false,    //we can cancel or delete the notification
+    ongoing: false, //make the notification don't popup
+    autoCancel: false, //we can cancel or delete the notification
   );
   final iOSChannelSpecifics = IOSNotificationDetails();
   return NotificationDetails(androidChannelSpecifics, iOSChannelSpecifics);
@@ -53,3 +58,91 @@ Future _showNotification(
   int id = 0,
 }) =>
     notifications.show(id, title, body, type);
+
+//Future<NotificationDetails> _icon(BuildContext context, Image icon) async {
+//  final iconPath = await saveImage(context, icon);
+//
+//  final androidPlatformChannelSpecifics = AndroidNotificationDetails(
+//    'big text channel id',
+//    'big text channel name',
+//    'big text channel description',
+//    largeIcon: iconPath,
+//    largeIconBitmapSource: BitmapSource.FilePath,
+//  );
+//  return NotificationDetails(androidPlatformChannelSpecifics, null);
+//}
+//
+//Future showIconNotification(
+//  BuildContext context,
+//  FlutterLocalNotificationsPlugin notifications, {
+//  @required String title,
+//  @required String body,
+//  @required Image icon,
+//  int id = 0,
+//}) async =>
+//    notifications.show(id, title, body, await _icon(context, icon));
+
+////----
+/*
+Future<NotificationDetails> _image(BuildContext context, Image picture) async {
+  final picturePath = await saveImage(context, picture);
+
+  final bigPictureStyleInformation = BigPictureStyleInformation(
+    picturePath,
+    BitmapSource.FilePath,
+  );
+
+  final androidPlatformChannelSpecifics = AndroidNotificationDetails(
+    'big text channel id',
+    'big text channel name',
+    'big text channel description',
+    style: AndroidNotificationStyle.BigPicture,
+    styleInformation: bigPictureStyleInformation,
+  );
+  return NotificationDetails(androidPlatformChannelSpecifics, null);
+}
+
+Future showImageNotification(
+  BuildContext context,
+  FlutterLocalNotificationsPlugin notifications, {
+  @required String title,
+  @required String body,
+  @required Image picture,
+  int id = 0,
+}) async =>
+    notifications.show(id, title, body, await _image(context, picture));
+
+Future<NotificationDetails> _imageAndIcon(
+    BuildContext context, Image picture, Image icon) async {
+  final iconPath = await saveImage(context, icon);
+  final picturePath = await saveImage(context, picture);
+
+  final bigPictureStyleInformation = BigPictureStyleInformation(
+    picturePath,
+    BitmapSource.FilePath,
+    largeIcon: iconPath,
+    largeIconBitmapSource: BitmapSource.FilePath,
+  );
+
+  final androidPlatformChannelSpecifics = AndroidNotificationDetails(
+    'big text channel id',
+    'big text channel name',
+    'big text channel description',
+    style: AndroidNotificationStyle.BigPicture,
+    styleInformation: bigPictureStyleInformation,
+  );
+  return NotificationDetails(androidPlatformChannelSpecifics, null);
+}
+
+Future showIconAndImageNotification(
+  BuildContext context,
+  FlutterLocalNotificationsPlugin notifications, {
+  @required String title,
+  @required String body,
+  @required Image picture,
+  @required Image icon,
+  int id = 0,
+}) async =>
+    notifications.show(
+        id, title, body, await _imageAndIcon(context, picture, icon));
+*/
